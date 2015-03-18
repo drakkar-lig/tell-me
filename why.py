6d2dbe95730bc623c8ceda2222fe42255447f889
Tristan <tristan@skyros.imag.fr>
2015-03-18 14:54:42 +0100

    First commit with file

---
+++ why.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import urllib, urllib2, sys, json
+from bs4 import BeautifulSoup
+from StringIO import StringIO
+import gzip
+
+question = "site:stackoverflow.com "+" ".join(sys.argv[1:])
+
+base = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&'
+query = urllib.urlencode({'q' : question})
+response = urllib.urlopen(base + query).read()
+data = json.loads(response)
+question_url = data['responseData']['results'][0]['url']
+
+question_id=[ int(word) for word in question_url.split('/') if word.isdigit() ][0]
+
+query="https://api.stackexchange.com/2.2/questions/"+str(question_id)+"?order=desc&site=stackoverflow&filter=withbody"
+
+request = urllib2.Request(query)
+request.add_header('Accept-encoding', 'gzip')
+response = urllib2.urlopen(request)
+if response.info().get('Content-Encoding') == 'gzip':
+      buf = StringIO( response.read())
+      f = gzip.GzipFile(fileobj=buf)
+      data = f.read()
+
+datajson=json.loads(data)
+question=datajson["items"][0]["body"]
+print "---------------------------------------------"
+
+print question
+
+query2="https://api.stackexchange.com/2.2/questions/"+str(question_id)+"/answers?order=desc&min=0&max=0&sort=votes&site=stackoverflow&filter=withbody"
+
+request = urllib2.Request(query)
+request.add_header('Accept-encoding', 'gzip')
+response = urllib2.urlopen(request)
+if response.info().get('Content-Encoding') == 'gzip':
+      buf = StringIO( response.read())
+      f = gzip.GzipFile(fileobj=buf)
+      data = f.read()
+
+datajson=json.loads(data)
+answer = datajson["items"][0]["body"]
+
+print "---------------------------------------------"
+print answer
+
+html=urllib.urlopen(question_url).read()
+soup=BeautifulSoup(html)
