a861b9d8391c53b645e7eb3cf5ae3e5794575bab
Tristan <tristan@skyros.imag.fr>
2015-03-19 11:12:47 +0100

    Correction bug dans l'utilisation de pandoc et groffer, séparation fonction ask google

--- why.py
+++ why.py
@@ -8,15 +8,19 @@ import os
 from subprocess import Popen, PIPE, STDOUT
 import time
 
-question = "site:stackoverflow.com "+" ".join(sys.argv[1:])
 
-base = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&'
-query = urllib.urlencode({'q' : question})
-response = urllib.urlopen(base + query).read()
-datajson = json.loads(response)
-question_url = datajson['responseData']['results'][0]['url']
+def ask_google(site,question):
 
-question_id=[ int(word) for word in question_url.split('/') if word.isdigit() ][0]
+    question = "site:"+site+" "+" ".join(question)
+
+    base = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&'
+    query = urllib.urlencode({'q' : question})
+    response = urllib.urlopen(base + query).read()
+    datajson = json.loads(response)
+    question_url = datajson['responseData']['results'][0]['url']
+
+    question_id=[ int(word) for word in question_url.split('/') if word.isdigit() ][0]
+    return question_id
 
 def get_details_for_question(question_id):
     # we use the rss feed (no usage limitations)
@@ -29,6 +33,8 @@ def get_details_for_question(question_id):
     answers_per_rank = { int(entry.find('re:rank').string) : \
             entry.summary.string for entry in feed_entries[1:] }
     return question_html, answers_per_rank
+
+
 
 #print question_id
 #query="https://api.stackexchange.com/2.2/questions/"+str(question_id)+"?order=desc&site=stackoverflow&filter=withbody"
@@ -86,17 +92,18 @@ def get_details_for_question(question_id):
 #if backoff>0:
 #  time.sleep()
 
+question_id=ask_google("stackoverflow.com",sys.argv[1:])
 question, answers_per_rank = get_details_for_question(question_id)
 best_answer = answers_per_rank[max(answers_per_rank.keys())]
 
 #p = Popen(['w3m','-T', 'text/html','-dump'],stdin=PIPE)  
 #p.communicate(question+"<p>-------<b>toto</b>-----------</p>"+answer)
-p = Popen(['pandoc','-f', 'html', '-t','groff'],stdin=PIPE,stdout=PIPE)  
+p = Popen(['pandoc','-f', 'html', '-t','man'],stdin=PIPE,stdout=PIPE)  
 toto=p.communicate(question+"<p>-------<b>toto</b>-----------</p>"+best_answer)
 print toto[0]
 groff=""
-p2 = Popen(['less'],stdin=PIPE)  
-p2.communicate(toto[0].replace(".PP",".HP"))
+p2 = Popen(['groffer','--tty'],stdin=PIPE)  
+p2.communicate(toto[0])
 
 #p2 = Popen(['groffer','--mode','tty'],stdin=PIPE)  
 #p2.communicate(toto[0].replace(".PP",".HP"))
